6d3d50343fe40691d6d90fa8859806f5c0beb0d7
rvankoten <r.a.m.vankoten@uva.nl>
2019-05-06 15:39:00 +0200

    implemented signin in
    App can now sign in at the server.
    TODO: add username, now using current trykey api

--- DataNoseScanner/DataNoseScanner/LoginPage.xaml.cs
+++ DataNoseScanner/DataNoseScanner/LoginPage.xaml.cs
@@ -7,18 +7,58 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using DataNoseScanner.Common;
+using DataNoseScanner.DataNose;
+
 namespace DataNoseScanner
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private Settings settings = null;
+
         public LoginPage()
         {
             InitializeComponent();
+
+            settings = new Settings();
+            if (settings.SignedUp == true)
+                Task.Run(() => LoginSuccessfull());
         }
 
         private async void btnLogin_Clicked(object sender, EventArgs e)
         {
+            string sUser = User.Text;
+            string sPass = Password.Text;
+            string api_url = settings.server + settings.server_api;
+
+            DataNoseConnector DNC = new DataNoseConnector(api_url, sUser, sPass);
+            DataNoseKeyResponse response = await DNC.tryKey();
+            if ((response != null) && (response.status == "valid-key"))
+            {
+                settings.UserID = sUser;
+                settings.UserPass = sPass;
+                settings.SignedUp = true;
+                await LoginSuccessfull(response.message);
+            }
+            else
+                LoginFailed();
         }
+
+        private async Task LoginSuccessfull(string sMessage)
+        {
+            await LoginSuccessfull();
+        }
+
+        private async Task LoginSuccessfull()
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private void LoginFailed()
+        {
+            
+        }
+
     }
 }
